aaf17184236344614e590e1d36843a8ff6cb8d2f
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:41:05 +1300

    Converted to PSR-2

--- code/Methods/OtherCategories.php
+++ code/Methods/OtherCategories.php
@@ -8,22 +8,27 @@
  * @author Mellisa Hankins <mell@milkywaymultimedia.com.au>
  */
 
-class OtherCategories implements Contract {
-	public function title($buyable) {
-		return 'Other Categories';
-	}
+class OtherCategories implements Contract
+{
+    public function title($buyable)
+    {
+        return 'Other Categories';
+    }
 
-	public function getFormFields($buyable) {
-		return \CompositeField::create(
-			\LiteralField::create('Recommended_FindBy-OtherCategory-Message', '<p class="message field desc selectionGroup-desc">' . _t('Product.Recommended_FindBy-OtherCategory-Message', 'Recommended products will be pulled from the selected categories') . '</p>'),
-			\TreeMultiselectField::create('Recommended_Categories', _t('Product.Categories', 'Categories'), 'ProductCategory', 'ID', 'MenuTitle')
-		);
-	}
+    public function getFormFields($buyable)
+    {
+        return \CompositeField::create(
+            \LiteralField::create('Recommended_FindBy-OtherCategory-Message', '<p class="message field desc selectionGroup-desc">' . _t('Product.Recommended_FindBy-OtherCategory-Message', 'Recommended products will be pulled from the selected categories') . '</p>'),
+            \TreeMultiselectField::create('Recommended_Categories', _t('Product.Categories', 'Categories'), 'ProductCategory', 'ID', 'MenuTitle')
+        );
+    }
 
-	public function getList($buyable, $limit = 4) {
-		if(!$buyable->get()->filter('ParentID', $buyable->Recommended_Categories()->column('ID'))->exclude('ID', $buyable->ID)->exists())
-			return null;
+    public function getList($buyable, $limit = 4)
+    {
+        if (!$buyable->get()->filter('ParentID', $buyable->Recommended_Categories()->column('ID'))->exclude('ID', $buyable->ID)->exists()) {
+            return null;
+        }
 
-		return $buyable->get()->filter('ParentID', $buyable->Recommended_Categories()->column('ID'))->exclude('ID', $buyable->ID);
-	}
-} 
+        return $buyable->get()->filter('ParentID', $buyable->Recommended_Categories()->column('ID'))->exclude('ID', $buyable->ID);
+    }
+}
